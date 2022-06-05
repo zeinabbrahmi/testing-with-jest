0bd25efe70bd4de9cbf695ed9f67d52d495c1ef9
zeinabbrahmi <zeinab.brahmi@gmail.com>
2022-06-05 13:50:10 +0200

    added stack.js

---
+++ src/stack.js
@@ -0,0 +1,18 @@
+var _ = require('underscore');
+
+let stack = [];
+
+// Lägger ett element överst i stacken
+exports.push = function (x) {
+    stack.push(x);
+};
+
+// Returnerar det översta elementet i stacken och tar bort det
+exports.pop = function () {
+    return stack.pop();
+}
+
+// Returnerar det översta elementet i stacken
+exports.peek = function () {
+    return stack[0]; // Det här är medvetet felaktigt
+}

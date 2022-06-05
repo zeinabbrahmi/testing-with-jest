046c5594c4721732b2fbde0b183ee3eea8edd373
zeinabbrahmi <zeinab.brahmi@gmail.com>
2022-06-05 14:02:36 +0200

    test fil

---
+++ tests/stack.test.js
@@ -0,0 +1,19 @@
+const stack = require('../src/stack');
+
+test('peek on empty stack returns undefined', () => {
+    expect(stack.peek()).toBeUndefined();
+});
+
+test('peek on stack with one element returns that element', () => {
+    stack.push(1);
+    expect(stack.peek()).toBeDefined();
+    expect(stack.peek()).toBe(1);
+});
+
+test('peek on stack with two or more elements returns the top element', () => {
+    stack.push(1);
+    stack.push("wow");
+    stack.push(42);
+    expect(stack.peek()).toBeDefined();
+    expect(stack.peek()).toBe(42);
+});

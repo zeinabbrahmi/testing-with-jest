2ccf74d88756b773cea9c42f5c08c3ae86bba8e5
zeinabbrahmi <zeinab.brahmi@gmail.com>
2022-06-05 14:04:57 +0200

    fixed bug in stack

--- src/stack.js
+++ src/stack.js
@@ -14,5 +14,5 @@ exports.pop = function () {
 
 // Returnerar det översta elementet i stacken
 exports.peek = function () {
-    return stack[0]; // Det här är medvetet felaktigt
+    return _.last(stack);
 }
